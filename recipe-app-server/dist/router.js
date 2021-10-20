085b9c80c05802c232918ec2f3c0aa2fe61f532c
Miguel <rawredonion@gmail.com>
2021-10-19 20:45:28 -0400

    Remove `dist` folder from server tracking.

--- recipe-app-server/dist/router.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const express_1 = tslib_1.__importDefault(require("express"));
-exports.default = express_1.default.Router();
